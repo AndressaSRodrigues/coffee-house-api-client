4e77495ad533b890973c0665b84818211214bff9
AndressaSRodrigues <srandressa2@gmail.com>
2023-11-14 22:44:21 -0300

    Added reducer to users to manage states

--- coffee-house/src/components/Manage/UsersTable.tsx
+++ coffee-house/src/components/Manage/UsersTable.tsx
@@ -3,53 +3,77 @@ import { getUsers, deleteUsers } from '../../services/users';
 import { useAuth } from '../../context/AuthContext';
 import { CustomTable } from '../Shared/Table';
 import DeleteDialog from '../Shared/Dialog';
-import { useState, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
 
 export default function UsersTable() {
-  const [usersData, setUsersData] = useState<Users[]>([]);
+
+  interface State {
+    usersData: Users[];
+    isDeleteDialogOpen: boolean;
+    selectedUserId: string;
+  }
+  
+  interface Action {
+    type: string;
+    payload?: any;
+  }
+
+  const initialState = {
+    usersData: [],
+    isDeleteDialogOpen: false,
+    selectedUserId: '',
+  };
+
+  const reducer = (state: State, action: Action) => {
+    switch (action.type) {
+      case 'SET_USERS_DATA':
+        return { ...state, usersData: action.payload };
+      case 'OPEN_DELETE_DIALOG':
+        return { ...state, isDeleteDialogOpen: true, selectedUserId: action.payload };
+      case 'CLOSE_DELETE_DIALOG':
+        return { ...state, isDeleteDialogOpen: false, selectedUserId: '' };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState );
   const { token } = useAuth();
 
   useEffect(() => {
     getUsers(token)
       .then((data) => {
-        console.log(data)
-        setUsersData(data);
+        dispatch({ type: 'SET_USERS_DATA', payload: data });
       })
       .catch((error) => {
         console.error(error);
       });
   }, [token]);
 
+  const openDeleteDialog = (userId: string) => {
+    dispatch({ type: 'OPEN_DELETE_DIALOG', payload: userId });
+  };
+
   const deleteUser = (userId: string) => {
     deleteUsers(token, userId)
       .then(() => {
-        console.log('User deleted');
-        setUsersData(usersData.filter(user => user._id !== userId));
+        dispatch({ type: 'SET_USERS_DATA', payload: state.usersData.filter((user: Users) => user._id !== userId) });
       })
       .catch((error) => {
         console.error(error);
       });
   };
 
-  const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [selectedUserId, setSelectedUserId] = useState('');
-
-  const openDeleteDialog = (userId: string) => {
-    setSelectedUserId(userId);
-    setDeleteDialogOpen(true);
-  };
-
   const closeDeleteDialog = () => {
-    setSelectedUserId('');
-    setDeleteDialogOpen(false);
+    dispatch({ type: 'CLOSE_DELETE_DIALOG' });
   };
 
   const tableHeaders = ['email', 'role', 'options'];
 
   return (
     <>
-      <CustomTable data={usersData} headers={tableHeaders} onDelete={openDeleteDialog} />
-      <DeleteDialog open={isDeleteDialogOpen} onCancel={closeDeleteDialog} onDelete={() => deleteUser(selectedUserId)} />
+      <CustomTable data={state.usersData} headers={tableHeaders} onDelete={openDeleteDialog}/>
+      <DeleteDialog open={state.isDeleteDialogOpen} onCancel={closeDeleteDialog} onDelete={() => deleteUser(state.selectedUserId)} />
     </>
   );
 };
